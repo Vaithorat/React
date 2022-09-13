05114137728aaaed5f1e715ff72c2804a6cd51a3
vaithorat <vaibhavthorat141997@gmail.com>
2022-09-13 12:30:38 +0530

    v1.1 Fixed dark mode bugs

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -29,7 +29,7 @@ const handleExtraSpace = () =>{
   
   return (
     <>
-    <div className = "container">
+    <div className = "container" style = {{color: props.mode === "dark" ? "white" : "black"}}>
       <h1>
         {props.heading} 
       </h1>
@@ -37,6 +37,7 @@ const handleExtraSpace = () =>{
         <textarea
         value={text}
         className="form-control"
+        style = {{backgroundColor: props.mode === "dark" ? "#121212" : "#ffffff",color: props.mode === "dark" ? "white" : "black"}}
         id="exampleFormControlTextarea1"
         rows="8"
         onChange={handleOnChange}
@@ -47,7 +48,7 @@ const handleExtraSpace = () =>{
       <button className="btn btn-primary mx-3" onClick={handleClear}>Clear</button>
       <button className="btn btn-primary mx-3" onClick={handleExtraSpace}>Remove Extra Spaces</button>
     </div>
-    <div className="container my-2"> {/* my-2 adds margin of 2 */}
+    <div className="container my-2" style = {{color: props.mode === "dark" ? "white" : "black"}}> {/* my-2 adds margin of 2 */}
       <h2>Text Summary</h2>
       <p>{text.split(" ").length} Words in text and {text.length} characters in text</p>
       <p>This will take {text.split(" ").length*0.008} Minutes to read</p>
